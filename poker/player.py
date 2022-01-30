7ccb44206014eea2ff5c6514a778d31a2aa2bf1c
vincentyeungg <vincentyeung0809@gmail.com>
2022-01-30 02:55:33 -0500

    added player class and unit tests

---
+++ poker/player.py
@@ -0,0 +1,36 @@
+class Player:
+    """
+    Represents a player in a game of poker. A player will have some balance to bet if they wish, and their cards.
+    """
+
+    def __init__(self, name, hand):
+        self.name = name
+        self.hand = hand
+        self.balance = 100.0 # default balance for now, everyone gets this amount to play the game
+        self.play = True
+
+    def add_cards(self, cards):
+        self.hand.add_cards(cards)
+
+    def best_hand(self):
+        # return the name of the hand identified as the best hand
+        return self.hand.best_hand()[0]
+
+    def best_hand_cards(self):
+        # return the cards of the best identified as the best hand
+        return self.hand.best_hand()[1]
+
+    def wants_to_fold(self):
+        # if player chooses to fold during a betting round, they will be removed from the game
+        self.play = False
+
+    def place_bet(self, amount):
+        # take the amount from the current
+        # check if the current player has enough balance to make the bet
+        # if invalid amount, return False, else modify the balance and return True
+        if not amount <= 0:
+            if self.balance >= amount:
+                self.balance -= amount
+                return True
+
+        return False
---
+++ tests/test_player.py
@@ -0,0 +1,90 @@
+import pytest
+
+from poker.player import Player
+from poker.hand import Hand
+from poker.card import Card
+
+
+@pytest.fixture
+def testing_cards():
+    return [
+        Card(rank="3", suit="Clubs"),
+        Card(rank="7", suit="Clubs"),
+        Card(rank="8", suit="Clubs"),
+        Card(rank="8", suit="Hearts"),
+        Card(rank="9", suit="Clubs"),
+        Card(rank="10", suit="Diamonds"),
+        Card(rank="Jack", suit="Clubs")
+    ]
+
+@pytest.fixture
+def player():
+    hand = Hand()
+    name = "Test Player 1"
+
+    player = Player(name=name, hand=hand)
+
+    return player
+
+
+def test_create_player_and_store_name_and_hand_and_balance(testing_cards, player):
+    player.add_cards(testing_cards)
+
+    assert player.name == "Test Player 1"
+    assert player.hand.cards == testing_cards
+
+
+def test_player_will_play_initially(player):
+    assert player.play
+
+
+def test_player_can_add_cards_into_hand():
+    hand = Hand()
+    name = "Test Player 1"
+    player = Player(name=name, hand=hand)
+
+    cards = [Card(rank="2", suit="Clubs"), Card(rank="7", suit="Diamonds")]
+
+    player.add_cards(cards)
+
+    assert player.hand.cards == [Card(rank="2", suit="Clubs"), Card(rank="7", suit="Diamonds")]
+
+
+def test_obtain_best_hand_based_on_cards_from_collection(testing_cards, player):
+    player.add_cards(testing_cards)
+
+    # depends on the testing cards hand
+    assert player.best_hand() == "Flush"
+
+
+def test_obtain_cards_that_make_up_the_best_hand(testing_cards, player):
+    player.add_cards(testing_cards)
+
+    # depends on the testing cards hand
+    assert player.best_hand_cards() == [
+        Card(rank="3", suit="Clubs"),
+        Card(rank="7", suit="Clubs"),
+        Card(rank="8", suit="Clubs"),
+        Card(rank="9", suit="Clubs"),
+        Card(rank="Jack", suit="Clubs")
+    ]
+
+
+def test_player_wants_to_fold(player):
+    player.wants_to_fold()
+
+    # if a player folds, play flag set to false
+    assert player.play == False
+
+
+def test_player_makes_bet_with_valid_amount(player):
+    player.place_bet(50.0)
+    assert player.balance == 50.0
+
+
+def test_player_makes_bet_with_invalid_amount(player):
+    assert player.place_bet(200.0) == False
+
+
+def test_player_enters_invalid_amount(player):
+    assert player.place_bet(-50.0) == False
